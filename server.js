c1999e729d59ccac1756602f5d59e6b4248f46a8
Prolix <51418825+Prolix19@users.noreply.github.com>
2022-03-24 02:12:08 -0500

    Update server, create models

--- server.js
+++ server.js
@@ -36,3 +36,43 @@
 // WHEN I am idle on the site for more than a set time
 // THEN I am able to view comments but I am prompted to log in again before I can add, update, or delete comments
 
+const express = require("express");
+const routes = require("./controllers/");
+const sequelize = require("./config/connection");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({helpers});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: "mysecret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
+
+// Fire up routes
+app.use(routes);
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Connect to DB and run server
+sequelize.sync({force: false})
+.then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}`)
+    );
+});
